0f892b40e0a1833c7329a4c9ff43b5cf7d6f97f6
Vaibhaw_Office <vaibhawraj@yahoo.in>
2016-03-04 17:46:00 +0530

    Chain updated

--- core.js
+++ core.js
@@ -7,18 +7,29 @@
 		drawGrid : function(point1,point2){}
 	};
 	var Chain = {
+		FPS : 0,
+		MaxLifeSpan : 20,
 		Balls : [],
 		TotalBalls : 0,
+		Pointer : null,
 		init : function(){},
 		redraw : function(){},
 		update : function(){}
 	};
+	var pointer = function(location,color) {
+		this.location = location;
+		this.color = color;
+		this.destroy = function(){};
+		this.timeRemaining = 0;
+	}
 	var Ball = function(location,velocity,color){
 		this.explode = function(){};
+		this.destroy = function(){};
 		this.location = location;
+		this.color = color;
 		this.velocity = velocity;
 		this.update = function(){};		//Update Location, radius, color etc..
-		this.collided = function(){};   //Return true or false
+		this.collided = function(ball){};   //Return true or false
 		return this;
 	}; //ToDo Define Ball
 	var Vector = function(x,y){}; //ToDo Define Ball
